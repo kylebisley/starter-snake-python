6e5b5bf342157f83078ac5f0ea46476357b99f8b
Kyle Bisley <kylebisley@gmail.com>
2020-03-04 15:52:23 -0800

    switched the x,y

--- app/main.py
+++ app/main.py
@@ -258,11 +258,11 @@ def sumPathWeight(path, pathBoard):
         print ("step")
         print (step)
         print ("tile weight")
-        print pathBoard[step[0]] [step[1]]
-        if (pathBoard[step[0]][step[1]] <= 0):
+        print pathBoard[step[1]] [step[0]]
+        if (pathBoard[step[1]][step[0]] <= 0):
             print("doom")
             return "doom"
-        weight = weight + pathBoard[step[0]] [step[1]]
+        weight = weight + pathBoard[step[1]] [step[0]]
     print("weight")
     print(weight)
     return weight
