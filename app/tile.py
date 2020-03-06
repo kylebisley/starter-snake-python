4eec57388eb417b85f5cc9243d914f1f662ab4f8
DimaM <dima.m99@gmail.com>
2020-03-06 10:44:26 -0800

    Formatting

--- app/tile.py
+++ app/tile.py
@@ -1,30 +1,29 @@
-class Tile():
+class Tile:
     """
     Attributes:
-        x (integer): the x coordinate, should line up with our other methods
-        y (integer): the y coordinate etc
+        _x (integer): the x coordinate, should line up with our other methods
+        _y (integer): the y coordinate etc
         visited (boolean): whether or not a tile has been visited, useful for some algorithms
         cost: the cost for a-star to travers a tile, can be used to determine if a tile is pathable
 
     Object Methods:
-        getCoord(): int list, returns a list of the x position, then the y position
-        getX(): int, returns the x coordinate of the tile
-        getY(): int, returns the y coordinate of the tile
+        get_coord(): int list, returns a list of the x position, then the y position
+        get_x(): int, returns the x coordinate of the tile
+        get_y(): int, returns the y coordinate of the tile
         visit(): void, sets visited to true
-        getVisited(): boolean, returns current state of visited
-        getCost(): int, returns cost attribute of the tile
+        get_visited(): boolean, returns current state of visited
+        get_cost(): int, returns cost attribute of the tile
     """
 
-    def __init__(self, xCoord, yCoord, pathCost, is_food, debug_char):
-        self._x = xCoord
-        self._y = yCoord
-        self.cost = pathCost
+    def __init__(self, x_coord, y_coord, path_cost, is_food, debug_char):
+        self._x = x_coord
+        self._y = y_coord
+        self.cost = path_cost
         self._is_food = is_food
         self._debug_char = debug_char
         self.visited = False
 
-
-    #Getters and setters
+    # Getters and setters
     
     def get_x(self):
         return self._x
@@ -40,15 +39,13 @@ class Tile():
     
     def get_cost(self):
         return self.cost
-    
 
-    '''
-    doesn't change "walls" and won't make a pathable tile unpathable
-    '''
+    # doesn't change "walls" and won't make a pathable tile un-pathable
+
     def modify_cost(self, change_by):
-        if self.getCost() < 1:
+        if self.get_cost() < 1:
             return
-        if self.getCost() + change_by < 1:
+        if self.get_cost() + change_by < 1:
             self.cost = 1
             return
         self.cost += change_by
