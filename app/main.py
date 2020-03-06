0cc1015c33371e484e6647edbdc7eb06182e7296
Dominique Hervochon <dhervochon@hotmail.ca>
2020-03-05 17:59:11 -0800

    started on object oriented stuff, etc

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 import os
 import bottle
 import parseBoard
+import tile
 
 from api import ping_response, start_response, move_response, end_response
 from pathfinding.core.grid import Grid
