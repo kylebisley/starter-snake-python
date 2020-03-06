c37dd63428b1c2c020fc77ee54e93a7a92a32d32
TateM <TateMowbray@outlook.com>
2020-03-06 14:28:05 -0800

    redid merge

--- app/board.py
+++ app/board.py
@@ -5,14 +5,12 @@ class Board:
 
     # TODO: add functionality to ignore tails if their snake isn't growing
     # this turn
-    def __init__(self, j_data, path_board):
+    def __init__(self, j_data, path_board, char_board):
         self._board_width = len(j_data["board"]["width"])
         self._board_height = len(j_data["board"]["height"])
         self._the_board = [[None for x in range(self._board_width)] for y in
                            range(self._board_height)]
         self.food_tiles = []
-
-        char_board = board_to_array(j_data)
 
         for y in range(len(path_board)):
             for x in range(len(path_board[0])):
