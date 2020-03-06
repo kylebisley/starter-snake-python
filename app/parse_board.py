28bebb39e29cc361607b6d1b27052845a0c011e3
TateM <TateMowbray@outlook.com>
2020-03-06 14:58:04 -0800

    added longestPath

--- app/parse_board.py
+++ app/parse_board.py
@@ -232,52 +232,54 @@ def display(converted_board, integer_board):
         print()
 
 
-def look_from_here(the_board, the_tile, j_data):
-    '''
-    finds all tiles reachable on a board from a given tile, and the walls surrounding them, treats our head as
-    a wall
-    Args:
-        the_board (board): a board object
-        the_tile (tile): the tile object we want to search from
-        j_data (dict): the converted json data initially given to us
-    Returns:
-        a list of two 1d lists, the first is all the tiles that are possible to path to from the passed in tile,
-        the second is a list of tiles that form a wall around the pathable area
-    '''
-    tile_is_head = True
-    head_cost = the_tile.get_cost()
-    head = j_data["you"]["body"][0]
-    if not ((the_tile.get_x() == head["x"]) and (the_tile.get_y() == head["y"])):
-        tile_is_head = False
-        the_board.get_tile_at(head["x"], head["y"]).set_cost(-1)
+def DFS(pathableTiles, v, walls_around, seen=None, path=None):
+    # Build graph dictionary
+    G = copy.deepcopy(pathableTiles)
+    for (s, t) in walls_around:
+        G[s].append(t)
+        G[t].append(s)
 
+    # Run DFS, compute metrics
+    all_paths = DFS(G, '1')
+    max_len = max(len(p) for p in all_paths)
+    max_paths = [p for p in all_paths if len(p) == max_len]
 
-    new_viable_tiles = [the_tile]
-    blocking_tiles = []
-    pathable_tiles = []
-    
-    while (len(new_viable_tiles) > 0):
-        #deal with the next tile we are examining
-        check_next = new_viable_tiles.pop()
+    if seen is None:
+        seen = []
+    if path is None:
+        path = [v]
 
-        if (check_next.getCost() < 1):
-            blocking_tiles.append(check_next)
-            continue #go to next iteration if it's a wall, we don't care about it after this step
-        else:
-            pathable_tiles.append(check_next)
+    seen.append(v)
 
-        neighbours = the_board.find_neighbours(check_next)
+    paths = []
+    for t in G[v]:
+        if t not in seen:
+            t_path = path + [t]
+            paths.append(tuple(t_path))
+            paths.extend(DFS(G, t, seen[:], t_path))
+    # Output
+    print("All Paths:")
+    print(all_paths)
+    print("Longest Paths:")
+    for p in max_paths:
+        print("  ", p)
+    return paths
 
-        for t in neighbours:
-            if (not t.get_visited()):
-                new_viable_tiles.append(t)
-                t.set_visited(True)
-        
-    for x in range(the_board.get_width()):
-        for y in range(the_board.get_height()):
-            the_board.get_tile_at(x, y).set_visited(False)
-
-    if not tile_is_head:
-        the_board.get_tile_at(head["x"], head["y"]).set_cost(head_cost)
-    
-    return [pathable_tiles, blocking_tiles]
+# def look_from_here(the_board, the_tile, j_data):
+#     '''
+#     finds all tiles reachable on a board from a given tile, and the walls surrounding them, treats our head as
+#     a wall
+#     Args:
+#         the_board (board): a board object
+#         the_tile (tile): the tile object we want to search from
+#         j_data (dict): the converted json data initially given to us
+#     Returns:
+#         a list of two 1d lists, the first is all the tiles that are possible to path to from the passed in tile,
+#         the second is a list of tiles that form a wall around the pathable area
+#     '''
+#     tile_is_head = True
+#     head_cost = the_tile.get_cost()
+#     head = j_data["you"]["body"][0]
+#     if not ((the_tile.get_x() == head["x"]) and (the_tile.get_y() == head["y"])):
+#         tile_is_head = False
+#         the_board.get_tile_at(head["x"], head["y"]).set_cost(-1)
