c3a47424f917e5ff76af125e961c722ef3fd3d98
Dominique Hervochon <dhervochon@hotmail.ca>
2020-03-06 18:56:52 -0800

    assorted bugfixes and switched debug to board obj

--- app/board.py
+++ app/board.py
@@ -7,10 +7,12 @@ class Board:
     def __init__(self, j_data, path_board, char_board):
         self._board_width = j_data["board"]["width"]
         self._board_height = j_data["board"]["height"]
-        self._the_board = [[None for x in range(self._board_width)] for y in range(self._board_height)]
+        self._the_board = []
         self.food_tiles = []
 
         for y in range(len(path_board)):
+
+            new_row = []
             for x in range(len(path_board[0])):
 
                 is_food = False
@@ -18,13 +20,15 @@ class Board:
                     is_food = True
 
                 new_tile = t.Tile(x, y, path_board[y][x], is_food, char_board[y][x])
-                self._the_board[y].append(new_tile)
+                new_row.append(new_tile)
+                
                 if new_tile.get_food():
                     self.food_tiles.append(new_tile)
+            
+            self._the_board.append(new_row)
 
     # Getters and setters
     def get_tile_at(self, x, y):
-        print("getting tile at " + x + y)
         return self._the_board[y][x]
 
     def get_board_width(self):
@@ -48,36 +52,35 @@ class Board:
                          str(food.get_y()) +
                          "/")
         return food_list
-
     
     def print_int_board(self):
         '''
         prints integer representation of the board, same as pathing values
         '''
+        print "Integer Board, Width:", str(self.get_board_height()), "Height:", str(self.get_board_width())
         for y in range(self.get_board_height()):
             for x in range(self.get_board_width()):
-                print(self.get_tile_at(x, y).get_cost() + " "),
-            print()
-        print()
+                print str(self.get_tile_at(x, y).get_cost()) + " ",
+            print ""
+        print ""
         
-
     def print_dima_board(self):
         '''
         prints character representation of all tiles on board
         '''
+        print "Dima board:"
         for y in range(self.get_board_height()):
             for x in range(self.get_board_width()):
-                print(self.get_tile_at(x, y).get_char() + " "),
-            print()
-        print()
+                print str(self.get_tile_at(x, y).get_char()) + " ",
+            print ""
+        print ""
 
     def dab(self):
         raise NotImplementedError
 
-
     # Other methods
 
-    #takes in a tile, and outputs a list of all tiles that are adjacent to it
+    # takes in a tile, and outputs a list of all tiles that are adjacent to it
     def find_neighbours(self, tile):
         neighbours = []
 
@@ -85,20 +88,19 @@ class Board:
         if((tile.get_y() > 0) and not self.get_tile_at(tile.get_x(), tile.get_y() - 1)):
             neighbours.append(self.get_tile_at(tile.get_x(), tile.get_y() - 1))
 
-        #look up
+        # look up
         if((tile.get_y() < self.get_height() - 1) and not self.get_tile_at(tile.get_x(), tile.get_y() + 1)):
             neighbours.append(self.get_tile_at(tile.get_x(), tile.get_y() + 1))
 
-        #look left
+        # look left
         if((tile.get_x() > 0) and not self.get_tile_at(tile.get_x() - 1, tile.get_y())):
             neighbours.append(self.get_tile_at(tile.get_x() - 1, tile.get_y()))
         
-        #look right
+        # look right
         if((tile.get_x() < self.get_width() - 1) and not self.get_tile_at(tile.get_x() + 1,tile.get_y())):
             neighbours.append(self.get_tile_at(tile.get_x() + 1,tile.get_y()))
         
         return neighbours
-
 
     def get_path_board(self):
         '''
